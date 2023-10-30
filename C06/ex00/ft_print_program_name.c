c29ea733116b09dfa0e7d4db2834d5c57270b65e
Rob Van Baelen <rvan-bae@a1-r2-p2.s19.be>
2023-10-30 17:26:14 +0100

    update

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvan-bae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 16:48:55 by rvan-bae          #+#    #+#             */
+/*   Updated: 2023/10/30 16:56:43 by rvan-bae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int	main(int argc, char ** argv)
+int	main(int argc, char **argv)
 {
 	int	len;
-	
+
 	len = 0;
-	
-	while(argv[0][len])
+	if (argc < 1)
+		return (0);
+	while (argv[0][len])
 	{
 		write(1, &argv[0][len], 1);
 		len++;
@@ -14,4 +27,3 @@ int	main(int argc, char ** argv)
 	write(1, "\n", 1);
 	return (0);
 }
-	
