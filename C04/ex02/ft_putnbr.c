9c3adcb2febb69950e5b3e01fc141067b3576ba8
Rob Van Baelen <rvan-bae@a1-r2-p2.s19.be>
2023-10-30 16:44:34 +0100

    update

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -6,38 +6,35 @@
 /*   By: rvan-bae <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 16:20:45 by rvan-bae          #+#    #+#             */
-/*   Updated: 2023/10/27 13:47:48 by rvan-bae         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:13:31 by rvan-bae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putnbr(int nb)
 {
-	char	str[50];
-	int		mod;
-	int		index;
-	char	neg;
-
-	index = 0;
-	neg = 0;
 	if (nb == -2147483648)
-		write(1, "-2147483648", 11);
+	{
+		write (1, "-2147483648",11);
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
 	else
 	{
-		if (nb < 0)
-		{
-			nb *= -1;
-			write(1, "-", 1);
-		}
-		while (nb)
-		{
-			mod = nb % 10;
-			str[index++] = mod + 48;
-			nb /= 10;
-		}
-		while (index--)
-			write(1, &str[index], 1);
+		ft_putchar(nb + 48);
 	}
 }
 /*
